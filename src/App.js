6faf6187bf34cb3fedf6f619d4bb00dbf7309e19
Mateus Paiva <matteusp@alu.ufc.br>
2023-03-09 16:27:17 -0300

    removendo arquivos não pertinentes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+
 import Banner from './componentes/Banner/Banner';
 
 function App() {
@@ -7,18 +6,6 @@ function App() {
     <div className="App">
       <Banner />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edite <code>src/App.js</code> e salve para recarregar.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
