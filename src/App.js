eaf7fd8a1d568b79c5c178dfa1c28128a80fc7d9
Mateus Paiva <matteusp@alu.ufc.br>
2023-03-10 23:52:13 -0300

    mudanças para arow function e remodelagem

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
-import Banner from './componentes/Banner/Banner';
+import Banner from './componentes/Banner';
+import CampoTexto from './componentes/CompoTexto';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <CampoTexto label='Nome' placeholder='Digite o seu nome'/>
+      <CampoTexto label='Cargo'  placeholder='Digite o seu cargo'/>
+      <CampoTexto label='Imagem'  placeholder='Informe o endereço da imagem'/>
       <header className="App-header">
       </header>
     </div>
