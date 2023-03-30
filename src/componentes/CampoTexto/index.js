f58e43c11e04257fb87e383a627dd83b3a2a35eb
Mateus Paiva <matteusp@alu.ufc.br>
2023-03-30 11:54:34 -0300

    usando useState para valor dos campos

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,9 +1,22 @@
+import { useState } from 'react'
 import './CampoTexto.css'
 const CampoTexto = (props) => {
+
+    //let valor = ''
+
+    //const [valor, setValor] = useState('')
+
+    const aoDigitado = (evento) => {
+        //setValor(evento.target.value)
+        //valor = evento.target.value
+        props.aoAlterado(evento.target.value)
+    }
+
+
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input placeholder={`${props.placeholder}...`}/>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={`${props.placeholder}...`}/>
         </div>
     )
 }
